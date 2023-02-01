ddaaa0a79858d3fde182dff81c40793654d5fcd8
0xDecentMaz <0xDecentMaz@gmail.com>
2023-02-01 13:39:32 +0100

    Add SQFlite functionality, create tables and read/write in main

--- daylog/lib/main.dart
+++ daylog/lib/main.dart
@@ -1,10 +1,59 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:sqflite/sqflite.dart';
 import 'package:path/path.dart';
-import 'dart:async';
 
-void main() {
+void main() async {
   runApp(const MyApp());
+
+  WidgetsFlutterBinding.ensureInitialized();
+  final database = openDatabase(
+    join(await getDatabasesPath(), 'daylog.db'),
+    onCreate: (db, version) {
+      return db
+          .execute(
+            'CREATE TABLE activities(id INTEGER PRIMARY KEY, activity TEXT)',
+          )
+          .then((_) => db.execute(
+                'CREATE TABLE log(id INTEGER PRIMARY KEY, activity TEXT, datetime TEXT)',
+              ));
+    },
+    version: 1,
+  );
+
+  Future<void> insertActivity(Activities activities) async {
+    final db = await database;
+
+    await db.insert(
+      'activities',
+      activities.toMap(),
+      conflictAlgorithm: ConflictAlgorithm.replace,
+    );
+  }
+
+  Future<List<Activities>> activities() async {
+    // Get a reference to the database.
+    final db = await database;
+
+    // Query the table for all The Dogs.
+    final List<Map<String, dynamic>> maps = await db.query('activities');
+
+    // Convert the List<Map<String, dynamic> into a List<Dog>.
+    return List.generate(maps.length, (i) {
+      return Activities(
+        id: maps[i]['id'],
+        activity: maps[i]['activity'],
+      );
+    });
+  }
+
+  // test an insert
+  var testActivity = const Activities(id: 0, activity: 'firstTest');
+  await insertActivity(testActivity);
+
+  // test a print
+  print(await activities());
 }
 
 class MyApp extends StatelessWidget {
@@ -37,28 +86,17 @@ class _MyHomePageState extends State<MyHomePage> {
 
   void _incrementCounter() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
       _counter++;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
@@ -76,7 +114,54 @@ class _MyHomePageState extends State<MyHomePage> {
         onPressed: _incrementCounter,
         tooltip: 'Increment',
         child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ),
     );
+  }
+}
+
+class Activities {
+  final int id;
+  final String activity;
+
+  const Activities({
+    required this.id,
+    required this.activity,
+  });
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'activity': activity,
+    };
+  }
+
+  @override
+  String toString() {
+    return 'Activities{id: $id, activity: $activity}';
+  }
+}
+
+class Log {
+  final int id;
+  final String activity;
+  final String datetime;
+
+  const Log({
+    required this.id,
+    required this.activity,
+    required this.datetime,
+  });
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'activity': activity,
+      'datetime': datetime,
+    };
+  }
+
+  @override
+  String toString() {
+    return 'Log{id: $id, activity: $activity, datetime: $datetime}';
   }
 }
